e8e7b6f48360851799b807fc2c6bbf60f4c77c1a
Student User <student@workstation.lab.example.com>
2021-05-06 17:21:54 -0400

    Add IP

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,10 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+  
+
+// Printing the stored address
+//
+ echo "Server IP Address is: $ip_server";
 ?>
